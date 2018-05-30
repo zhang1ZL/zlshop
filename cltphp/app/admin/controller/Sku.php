3f52a67dca97cbd5fdf99ae00f19431da38ee329
shop <228996907@qq.com>
2018-05-30 14:43:57 +0800

    项目
    部署

---
+++ cltphp/app/admin/controller/Sku.php
@@ -0,0 +1,25 @@
+<?php
+namespace app\admin\controller;
+use think\Db;
+use clt\Leftnav;
+use app\admin\model\Admin;
+use app\admin\model\AuthGroup;
+use app\admin\model\authRule;
+use think\Validate;
+/*
+ * 商品   属性   规格
+ * 
+ * */
+class Sku extends Common{
+	public function skuList(){
+		
+	}
+	public function addSku(){
+		
+	}
+	public function editSku(){
+		
+	}
+}
+
+?>
